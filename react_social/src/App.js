00cd290fcb49302237537c0d8f07bf3aa0d42fd0
owlsspell <arhipowael@gmail.com>
2021-10-15 16:10:32 +0300

    baseurl

--- react_social/src/App.js
+++ react_social/src/App.js
@@ -58,7 +58,7 @@ const AppContainer = compose(
 
 
 const SocialApp = (props) => {
-  return     (<BrowserRouter>
+  return     (<BrowserRouter basename={process.env.PUBLIC_URL} >
   <Provider store={store}>
     <AppContainer />
   </Provider>
